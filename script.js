f87b1f2d0dc6b32592fbcbc517c53ef0f3b755a9
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-06-22 15:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const makeChange = (c) => {
+	const coins = {
+		0: [p: 1]
+		1: [n: 5]
+		2: [d: 10]
+		3: [q: 25]
+	}
+	const amount = {}
+	for(let i=0; i=3;i++){
+		const [bill, val] = coins[i]
+		while(c >= val){
+			amount += [bill,val]
+			c -=coins[val]  
+		}
+	}
   // your name here
 };
 
